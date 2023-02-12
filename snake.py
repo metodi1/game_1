1a1dd3c86c7cd8bb0d4d22d84a3eb9e6c1b98116
metod1 <metodi.ganev@gmail.com>
2023-02-12 13:03:41 +0200

    adding my_snake function

--- snake.py
+++ snake.py
@@ -4,9 +4,11 @@ import pygame
 from pygame import event
 
 pygame.init()
+
+pygame.display.set_caption('Snake game first trail')
 display_width = 400
 display_height = 400
-sneak_block = 10
+snake_block = 10
 display_game = pygame.display.set_mode((display_width, display_height))
 
 white = (255, 255, 255)
@@ -16,17 +18,23 @@ red = (255, 0, 0)
 clock = pygame.time.Clock()
 
 
+def my_snake(snake_list, snake_block):
+    for x in snake_list:
+        pygame.draw.rect(display_game, blue, [x[0], x[1], snake_block, snake_block])
+
+
 def game():
     game_over = False
     snake_speed = 3
+    snake_lenght = 1
     x1 = 0
     y1 = 0
 
     x1_change = 0
     y1_change = 0
-
-    foodx = round(random.randrange(0, display_width - sneak_block) / 10) * 10
-    foody = round(random.randrange(0, display_width - sneak_block) / 10) * 10
+    snake_list = []
+    foodx = round(random.randrange(0, display_width - snake_block) / 10) * 10
+    foody = round(random.randrange(0, display_width - snake_block) / 10) * 10
 
     while not game_over:
 
@@ -54,13 +62,27 @@ def game():
         x1 += x1_change
         y1 += y1_change
         display_game.fill(white)
-        pygame.draw.rect(display_game, blue, [x1, y1, sneak_block, sneak_block])
-        pygame.draw.rect(display_game, red, [foodx, foody, sneak_block, sneak_block])
+
+        pygame.draw.rect(display_game, red, [foodx, foody, snake_block, snake_block])
+        snake_Head = []
+        snake_Head.append(x1)
+        snake_Head.append(y1)
+        snake_list.append(snake_Head)
+        if len(snake_list) > snake_lenght:
+            del snake_list[0]
+
+        for x in snake_list[:-1]:
+            if x == snake_Head:
+                game_close = True
+
+        my_snake(snake_list, snake_block)
+
         if x1 == foodx and y1 == foody:
-            print('Изядено')
-            foodx = round(random.randrange(0, display_width - sneak_block) / 10) * 10
-            foody = round(random.randrange(0, display_width - sneak_block) / 10) * 10
+            foodx = round(random.randrange(0, display_width - snake_block) / 10) * 10
+            foody = round(random.randrange(0, display_width - snake_block) / 10) * 10
             snake_speed += 1
+            snake_lenght += 1
+
         pygame.display.update()
         clock.tick(snake_speed)
 
